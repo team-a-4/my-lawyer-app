ec1c16a01b51927fd74005dc835e8a35fb5278b7
RFeiz <zief002@gmail.com>
2023-06-09 00:21:32 +0400

    Finished law details

--- lib/constitution/law/c_law_details.dart
+++ lib/constitution/law/c_law_details.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:my_lawyer/_lawswwww.dart';
+import 'package:my_lawyer/chat/chat.dart';
 import 'package:my_lawyer/models/law.dart';
+import 'package:my_lawyer/user.dart';
+import 'package:uuid/uuid.dart';
 
 class ConstitutionLawDetails extends StatefulWidget {
   ConstitutionLawDetails({Key? key}) : super(key: key);
@@ -23,57 +27,104 @@ class _ConstitutionLawDetailsState extends State<ConstitutionLawDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("Protection to right",
-                style: TextStyle(fontWeight: FontWeight.bold))),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: widget.currentLaw.length,
-                itemBuilder: (context, index) {
-                  return indentations(widget.currentLaw[index], index);
-                },
-              ),
-            ],
-          ),
-        ));
-  }
+      appBar: AppBar(
+          title: const Text("Protection to right",
+              style: TextStyle(fontWeight: FontWeight.bold))),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widget.currentLaw.length,
+              itemBuilder: (context, index) {
+                return Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      indentations(widget.currentLaw[index], index),
+                      const SizedBox(
+                        height: 7,
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 75.0),
+          ],
+        ),
+      ),
+      floatingActionButton: GestureDetector(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SizedBox(
+              width: 200.0, // Adjust the width as needed
+              child: FloatingActionButton(
+                onPressed: () {
+                  // create new document in chat collection with auto generated id
+                  var uuid = const Uuid();
+                  var id = 'X${uuid.v4().replaceAll('-', '').substring(0, 19)}';
 
-  Padding indentation0(String msg) {
-    return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          msg,
-          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ));
+                  FirebaseFirestore.instance.collection('chat').doc(id).set({
+                    'uid': currentId,
+                    'messages': [],
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Chat(
+                        chatID: id,
+                      ),
+                    ),
+                  );
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                backgroundColor: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            const Text(
+              'Learn More',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 
   Padding indentations(String msg, int i) {
     TextStyle textStyle;
+    EdgeInsetsGeometry padding;
 
     if (msg.startsWith("~~")) {
-      textStyle = const TextStyle(fontSize: 10, fontWeight: FontWeight.bold);
+      textStyle = const TextStyle(fontSize: 15);
+      padding = const EdgeInsets.only(left: 34.0, right: 5);
       widget.trimmedLaw.add(widget.currentLaw[i].substring(2));
     } else if (msg.startsWith("~")) {
-      textStyle = const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+      textStyle = const TextStyle(fontSize: 15);
+      padding = const EdgeInsets.only(left: 20.0, right: 5);
       widget.trimmedLaw.add(widget.currentLaw[i].substring(1));
     } else if (msg.startsWith("=")) {
-      textStyle = const TextStyle(fontSize: 5, fontWeight: FontWeight.bold);
-      widget.trimmedLaw.add(widget.currentLaw[i].substring(1));
+      textStyle = const TextStyle(fontSize: 8, fontWeight: FontWeight.bold);
+      padding = const EdgeInsets.only(left: 50.0, right: 5);
+      widget.trimmedLaw.add("[${widget.currentLaw[i].substring(1)}]");
     } else {
-      textStyle = const TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+      textStyle = const TextStyle(fontSize: 15);
+      padding = const EdgeInsets.only(left: 8.0, right: 5);
       widget.trimmedLaw.add(widget.currentLaw[i]);
     }
 
     return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        widget.trimmedLaw[i],
-        style: textStyle,
-      ),
+      padding: padding,
+      child: Text(widget.trimmedLaw[i], style: textStyle),
     );
   }
 }
